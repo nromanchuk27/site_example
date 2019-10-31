fb27f00b2f905271662ffa33efab09f7b4c5b70f
Nazar <romanenko1927@gmail.com>
2019-10-31 19:52:42 +0200

    Updates

---
+++ precache-manifest.d3ed0d8e1a7df66fc65bd076d903e834.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ca2880e7cdefa200d3ceb96dcf8e84",
+    "url": "/site_example/index.html"
+  },
+  {
+    "revision": "2e945dc6ff8af3b820ca",
+    "url": "/site_example/static/css/main.8549b4a1.chunk.css"
+  },
+  {
+    "revision": "2af07ab55b3e83088209",
+    "url": "/site_example/static/js/2.f9a47dfe.chunk.js"
+  },
+  {
+    "revision": "2e945dc6ff8af3b820ca",
+    "url": "/site_example/static/js/main.22b230a4.chunk.js"
+  },
+  {
+    "revision": "69a5abf84651d5770456",
+    "url": "/site_example/static/js/runtime-main.cdde85b7.js"
+  },
+  {
+    "revision": "f2cf5fbb6bead909c75f6c0bfc8e1ad9",
+    "url": "/site_example/static/media/about-background.f2cf5fbb.png"
+  },
+  {
+    "revision": "e30a80002afae67edc32d5eb9df61c32",
+    "url": "/site_example/static/media/about-history.e30a8000.jpg"
+  },
+  {
+    "revision": "87620fb4bd9a3944f1986b2ee1a9b899",
+    "url": "/site_example/static/media/about-philosophy.87620fb4.jpg"
+  },
+  {
+    "revision": "fcf7411f08445bbda908a9aeacc18ef4",
+    "url": "/site_example/static/media/about-winners.fcf7411f.jpg"
+  },
+  {
+    "revision": "6c9ca42310f42a1b8d7877a2a7734b52",
+    "url": "/site_example/static/media/contact-background.6c9ca423.png"
+  },
+  {
+    "revision": "348e7a839dd1489a5d87929f2ee837af",
+    "url": "/site_example/static/media/intro-background.348e7a83.png"
+  },
+  {
+    "revision": "7cf659329fbda17f1742902c872eb4d1",
+    "url": "/site_example/static/media/work-alex.7cf65932.jpg"
+  },
+  {
+    "revision": "ee866377766a047a5784005aa2f395c3",
+    "url": "/site_example/static/media/work-metiew.ee866377.jpg"
+  },
+  {
+    "revision": "6ad08a98c2c452d29f44f96a4fb93c62",
+    "url": "/site_example/static/media/work-victory.6ad08a98.jpg"
+  }
+]);
